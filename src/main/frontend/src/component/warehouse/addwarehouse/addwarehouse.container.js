c4cf0c6bd7cb51ec26129c60fa0d88a0c88790ad
여운 <yeoun0570@gmail.com>
2024-09-30 18:34:58 +0900

    :recycle: 사용한 요소 초기화 추가

--- src/main/frontend/src/component/warehouse/addwarehouse/addwarehouse.container.js
+++ src/main/frontend/src/component/warehouse/addwarehouse/addwarehouse.container.js
@@ -28,16 +28,17 @@ export default function Warehouse() {
       const responseData = await post("/storage/register_storage", storageDTO);
       setData(responseData); // 상태에 데이터 저장
       console.log(responseData);
+
+      // 상태 초기화 (옵션)
+      setStorageName("");
+      setAddress("");
+      setAddressDetail("");
+      setZipcode("");
+      setStorageArea("");
     } catch (error) {
       console.error("데이터를 가져오는 중 오류가 발생했습니다:", error);
     }
   };
-
-   // 컴포넌트가 렌더링할 때 특정 동작을 수행하는 useEffect로 컴포넌트가 처음 렌더링될 때
-  // 데이터를 가져오는 fetchData를 호출
-  useEffect(() => {
-    fetchData(); // 데이터를 가져오는 함수 호출
-  }, []); // 의존성 배열이 빈 배열로 되어 있어, 컴포넌트가 처음 렌더링될 때 한 번만 실행됨
 
   return (
     <div>
